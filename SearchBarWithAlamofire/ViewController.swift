cd194d3f07df09ca0707cd7b41b4ef906f299f38
Jakkawad <admin@all2sale-iMac.local>
2016-05-16 16:46:42 +0700

    Edit

--- SearchBarWithAlamofire/ViewController.swift
+++ SearchBarWithAlamofire/ViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, UISearchDisplayDelegate {
 
     @IBOutlet var tableView: UITableView!
     
     var dataArray = NSArray()
+    
+    var productArray = NSArray()
+    var filteredProduct = NSArray()
     
     func loadData() {
         Alamofire.request(.POST, "https://www.all2sale.com/sendmail/testfunction/json/apitest.php", parameters: ["api":"category","category":"all"])
@@ -23,8 +26,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                 //print(response.data)     // server data
                 //print(response.result)   // result of response serialization
                 self.dataArray = response.result.value as! NSArray
+                //self.tableView.reloadData()
+                //print(self.dataArray.count)
+                //self.productArray = self.dataArray
+                //print(self.productArray)
                 self.tableView.reloadData()
-                print(self.dataArray)
         }
     }
 
@@ -34,19 +40,61 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        //return dataArray.count
+        //print(dataArray.count)
+        //return 10
+        
+        if (tableView == self.searchDisplayController?.searchResultsTableView) {
+            //print(filteredProduct.count)
+            return self.filteredProduct.count
+        } else {
+            //return self.productArray.count
+            return self.dataArray.count
+        }
+        
+        //return self.dataArray.count
+    
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell0 = tableView.dequeueReusableCellWithIdentifier("tableCell0")
-        cell0?.textLabel?.text = "AAA"
-        cell0?.detailTextLabel?.text = "BBB"
+        //cell0?.textLabel?.text = "AAA"
+        //cell0?.detailTextLabel?.text = "BBB"
+        //return cell0!
+        let item = self.dataArray[indexPath.row] as? NSDictionary
+        
+        let pn = item?.objectForKey("CategoryName") as? String
+        var pa = pn
+        self.productArray += item?.objectForKey("CategoryName") as? String
+        //var productDataArray = ProductItem()
+        if (tableView == self.searchDisplayController?.searchResultsTableView) {
+            //productDataArray = self.filteredProduct[indexPath.row] as! NSArray
+            //productDataArray = self.filteredProduct[indexPath.row] as? String
+            //productDataArray = self.filteredProduct[indexPath.row]
+            //print("AA")
+            //cell0?.textLabel?.text = item?.objectForKey("CategoryName") as? String
+            pa = self.filteredProduct[indexPath.row] as! String
+            
+        } else {
+            //productDataArray = self.dataArray[indexPath.row] as! NSArray
+            //print("BB")
+            cell0?.textLabel?.text = item?.objectForKey("CategoryName") as? String
+            
+        }
+        
+        //cell0?.textLabel?.text = item?.objectForKey("CategoryName") as? String
+        //let pn = item?.objectForKey("ProductName") as? String
+        //print(pn)
+        //cell0?.textLabel?.text = productDataArray
+        //cell0?.textLabel?.text = "LL"
+        
         return cell0!
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadData()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 

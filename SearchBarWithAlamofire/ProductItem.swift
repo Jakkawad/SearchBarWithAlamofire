bfaff7792a61e36db5bc985095306eb40a1104b8
Jakkawad <admin@all2sale-imac.local>
2016-05-16 15:34:04 +0700

    before searcher

---
+++ SearchBarWithAlamofire/ProductItem.swift
@@ -0,0 +1,13 @@
+//
+//  ProductItem.swift
+//  SearchBarWithAlamofire
+//
+//  Created by admin on 5/16/2559 BE.
+//  Copyright © 2559 All2Sale. All rights reserved.
+//
+
+import Foundation
+
+struct ProductItem {
+    let productNameItem:String
+}
--- SearchBarWithAlamofire/ViewController.swift
+++ SearchBarWithAlamofire/ViewController.swift
@@ -7,11 +7,46 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
+    
+    var dataArray = NSArray()
+    
+    func loadData() {
+        Alamofire.request(.POST, "https://www.all2sale.com/sendmail/testfunction/json/apitest.php", parameters: ["api":"category","category":"all"])
+            .responseJSON { response in
+                //print(response.request)  // original URL request
+                //print(response.response) // URL response
+                //print(response.data)     // server data
+                //print(response.result)   // result of response serialization
+                self.dataArray = response.result.value as! NSArray
+                self.tableView.reloadData()
+                print(self.dataArray)
+        }
+    }
+
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell0 = tableView.dequeueReusableCellWithIdentifier("tableCell0")
+        cell0?.textLabel?.text = "AAA"
+        cell0?.detailTextLabel?.text = "BBB"
+        return cell0!
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadData()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
